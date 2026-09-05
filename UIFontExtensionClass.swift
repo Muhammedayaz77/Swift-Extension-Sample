91042d0fe3e6bcc12cbda7f5aed89f1315e22fcd
Mohammed Ayaz <ayaz.job2010@gmail.com>
2026-09-05 13:34:33 +0530

    Fix UIFont extension API

--- UIFontExtensionClass.swift
+++ UIFontExtensionClass.swift
@@ -10,11 +10,11 @@ extension UIFont {
     /// Returns the font as bold italic.
     var boldItalic: UIFont { UIFont(descriptor: fontDescriptor.withSymbolicTraits([.traitBold,.traitItalic]) ?? fontDescriptor, size: 0) }
     /// Returns the font with a new point size.
-    func withSize(_ size: CGFloat) -> UIFont { withSize(size) }
+    func withFontSize(_ size: CGFloat) -> UIFont { UIFont(descriptor: fontDescriptor.withSize(size), size: 0) }
     /// Returns a smaller font by the supplied amount.
-    func smaller(by value: CGFloat = 1) -> UIFont { withSize(max(1, pointSize-value)) }
+    func smaller(by value: CGFloat = 1) -> UIFont { withFontSize(max(1, pointSize-value)) }
     /// Returns a larger font by the supplied amount.
-    func larger(by value: CGFloat = 1) -> UIFont { withSize(pointSize+value) }
+    func larger(by value: CGFloat = 1) -> UIFont { withFontSize(pointSize+value) }
     /// Returns the font point size.
     func getFontSize() -> CGFloat { pointSize }
     /// Returns the font family name.
@@ -25,17 +25,17 @@ extension UIFont {
     func isBoldFont() -> Bool { fontDescriptor.symbolicTraits.contains(.traitBold) }
     /// Returns whether the font is italic.
     func isItalicFont() -> Bool { fontDescriptor.symbolicTraits.contains(.traitItalic) }
-    /// Returns a semibold version when supported.
+    /// Returns a semibold system font at the current size.
     var semiBold: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .semibold) }
-    /// Returns a medium version when supported.
+    /// Returns a medium system font at the current size.
     var medium: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .medium) }
-    /// Returns a light version when supported.
+    /// Returns a light system font at the current size.
     var light: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .light) }
-    /// Returns an ultralight version when supported.
+    /// Returns an ultralight system font at the current size.
     var ultraLight: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .ultraLight) }
-    /// Returns a heavy version when supported.
+    /// Returns a heavy system font at the current size.
     var heavy: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .heavy) }
-    /// Returns a thin version when supported.
+    /// Returns a thin system font at the current size.
     var thin: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .thin) }
     /// Returns a regular system font at the current size.
     var regular: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .regular) }
