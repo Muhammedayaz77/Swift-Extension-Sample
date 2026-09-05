342fb7c951a00b0dc0689ab3dbc0100cae33dfa7
Mohammed Ayaz <ayaz.job2010@gmail.com>
2026-09-05 13:52:59 +0530

    Format UIFont extension code

--- UIFontExtensionClass.swift
+++ UIFontExtensionClass.swift
@@ -1,42 +1,99 @@
 import UIKit
 
 extension UIFont {
+
     /// Returns a scaled version of the font.
-    func scaled(scaleFactor: CGFloat) -> UIFont { UIFont(descriptor: fontDescriptor.withSize(pointSize * scaleFactor), size: 0) }
+    func scaled(scaleFactor: CGFloat) -> UIFont {
+        return UIFont(descriptor: fontDescriptor.withSize(pointSize * scaleFactor), size: 0)
+    }
+
     /// Returns the font as bold.
-    var bold: UIFont { UIFont(descriptor: fontDescriptor.withSymbolicTraits(.traitBold) ?? fontDescriptor, size: 0) }
+    var bold: UIFont {
+        return UIFont(descriptor: fontDescriptor.withSymbolicTraits(.traitBold) ?? fontDescriptor, size: 0)
+    }
+
     /// Returns the font as italic.
-    var italic: UIFont { UIFont(descriptor: fontDescriptor.withSymbolicTraits(.traitItalic) ?? fontDescriptor, size: 0) }
+    var italic: UIFont {
+        return UIFont(descriptor: fontDescriptor.withSymbolicTraits(.traitItalic) ?? fontDescriptor, size: 0)
+    }
+
     /// Returns the font as bold italic.
-    var boldItalic: UIFont { UIFont(descriptor: fontDescriptor.withSymbolicTraits([.traitBold,.traitItalic]) ?? fontDescriptor, size: 0) }
+    var boldItalic: UIFont {
+        return UIFont(descriptor: fontDescriptor.withSymbolicTraits([.traitBold, .traitItalic]) ?? fontDescriptor, size: 0)
+    }
+
     /// Returns the font with a new point size.
-    func withFontSize(_ size: CGFloat) -> UIFont { UIFont(descriptor: fontDescriptor.withSize(size), size: 0) }
+    func withSize(_ size: CGFloat) -> UIFont {
+        return UIFont(descriptor: fontDescriptor, size: size)
+    }
+
     /// Returns a smaller font by the supplied amount.
-    func smaller(by value: CGFloat = 1) -> UIFont { withFontSize(max(1, pointSize-value)) }
+    func smaller(by value: CGFloat = 1) -> UIFont {
+        return withSize(max(1, pointSize - value))
+    }
+
     /// Returns a larger font by the supplied amount.
-    func larger(by value: CGFloat = 1) -> UIFont { withFontSize(pointSize+value) }
+    func larger(by value: CGFloat = 1) -> UIFont {
+        return withSize(pointSize + value)
+    }
+
     /// Returns the font point size.
-    func getFontSize() -> CGFloat { pointSize }
+    func getFontSize() -> CGFloat {
+        return pointSize
+    }
+
     /// Returns the font family name.
-    func getFamilyName() -> String { familyName }
+    func getFamilyName() -> String {
+        return familyName
+    }
+
     /// Returns the font name.
-    func getFontName() -> String { fontName }
+    func getFontName() -> String {
+        return fontName
+    }
+
     /// Returns whether the font is bold.
-    func isBoldFont() -> Bool { fontDescriptor.symbolicTraits.contains(.traitBold) }
+    func isBoldFont() -> Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitBold)
+    }
+
     /// Returns whether the font is italic.
-    func isItalicFont() -> Bool { fontDescriptor.symbolicTraits.contains(.traitItalic) }
-    /// Returns a semibold system font at the current size.
-    var semiBold: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .semibold) }
-    /// Returns a medium system font at the current size.
-    var medium: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .medium) }
-    /// Returns a light system font at the current size.
-    var light: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .light) }
-    /// Returns an ultralight system font at the current size.
-    var ultraLight: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .ultraLight) }
-    /// Returns a heavy system font at the current size.
-    var heavy: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .heavy) }
-    /// Returns a thin system font at the current size.
-    var thin: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .thin) }
+    func isItalicFont() -> Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitItalic)
+    }
+
+    /// Returns a semibold version of the font.
+    var semiBold: UIFont {
+        return UIFont.systemFont(ofSize: pointSize, weight: .semibold)
+    }
+
+    /// Returns a medium version of the font.
+    var medium: UIFont {
+        return UIFont.systemFont(ofSize: pointSize, weight: .medium)
+    }
+
+    /// Returns a light version of the font.
+    var light: UIFont {
+        return UIFont.systemFont(ofSize: pointSize, weight: .light)
+    }
+
+    /// Returns an ultralight version of the font.
+    var ultraLight: UIFont {
+        return UIFont.systemFont(ofSize: pointSize, weight: .ultraLight)
+    }
+
+    /// Returns a heavy version of the font.
+    var heavy: UIFont {
+        return UIFont.systemFont(ofSize: pointSize, weight: .heavy)
+    }
+
+    /// Returns a thin version of the font.
+    var thin: UIFont {
+        return UIFont.systemFont(ofSize: pointSize, weight: .thin)
+    }
+
     /// Returns a regular system font at the current size.
-    var regular: UIFont { UIFont.systemFont(ofSize: pointSize, weight: .regular) }
+    var regular: UIFont {
+        return UIFont.systemFont(ofSize: pointSize, weight: .regular)
+    }
 }
